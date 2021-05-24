ef07e1ba0100fb8ea90707f2a6785e6eb3b40e73
Ïlya Chechevatov <uo.uo@list.ru>
2021-05-25 01:20:33 +0400

    ádd 2

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,8 +3,26 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
- 
-    return 0;
+    if ([ratingArray count] < 3) {
+        return 0;
+    }
+    NSMutableArray * temp = [[NSMutableArray alloc]init];
+    NSMutableArray * result = [[NSMutableArray alloc]init];
+    for (int i = 0; i <= [ratingArray count] - 3; i++) {
+        for (int j = i + 1; j <= [ratingArray count] - 2; j++) {
+            for (int k = j + 1; k <= [ratingArray count] - 1; k++) {
+                NSArray * arr = @[[ratingArray objectAtIndex:i],[ratingArray objectAtIndex:j],[ratingArray objectAtIndex:k]];
+                [temp addObject: arr];
+            }
+        }
+    }
+    for (NSArray<NSNumber*> * arr in temp) {
+        NSArray * sorted = [arr sortedArrayUsingSelector:@selector(compare:)];
+        if ([arr isEqualToArray: sorted] || [[[arr reverseObjectEnumerator]allObjects] isEqualToArray:sorted]) {
+            [result addObject:arr];
+        }
+    }
+    return [result count];
 }
 
 @end
